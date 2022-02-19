d8f0e2fd5a217fb725bcbce0caf5eddb09a00b91
ibrahimabir <ikabir2002@gmail.com>
2022-02-19 23:05:34 +0600

    second commit

--- basic.js
+++ basic.js
@@ -1,1 +1,21 @@
+// variable
 var deposite = 400;
+// condition 
+if (deposite > 500) {
+
+}
+if (deposite < 200) {
+
+}
+if (deposite == 500) {
+
+}
+if (deposite != 500) {
+
+}
+if (deposite <= 500) {
+
+}
+if (deposite >= 500) {
+
+}
